e4d6f525e221773a08a47e39e4cd0d4b42fe9c35
Astrid Boisson <astrid.bdc@gmail.com>
2020-05-26 15:23:17 +0200

    ok route

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   get 'about', to: 'pages#about'
   get 'contact', to: 'pages#contact'
-  ressources :flats, only: [:index, :show]
+  resources :flats, only: [:index, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
