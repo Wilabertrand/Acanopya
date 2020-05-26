313ca1f9cd56d962ed69cd1e99a5a40c9f38d9da
Astrid Boisson <astrid.bdc@gmail.com>
2020-05-26 17:16:41 +0200

    creation admin & policy trip controller

---
+++ app/policies/trip_policy.rb
@@ -0,0 +1,43 @@
+class TripPolicy < ApplicationPolicy
+  class Scope < Scope
+
+    def resolve
+      if user.admin?
+        scope.all #Trip.all
+      else
+        scope.where(user: user)
+      end
+    end
+  end 
+
+  def new?
+    create?
+  end
+
+  def show?
+    true
+  end
+
+  def create?
+    true
+  end
+
+  def edit?
+    update?
+  end
+
+  def update?
+    user_is_owner?
+  end
+
+  def destroy?
+    user_is_owner?
+  end
+
+  private
+
+  def user_is_owner?
+    record.user == user
+  end
+
+end
