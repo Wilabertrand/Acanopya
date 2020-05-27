4be52968b35f79e5af7879f0c0cdf9febae547ec
Astrid Boisson <astrid.bdc@gmail.com>
2020-05-27 18:17:27 +0200

    premiere ébauche

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,6 +5,28 @@ class PagesController < ApplicationController
     @trip = Trip.new
   end
 
+  def index
+    @flats =  Flat.where(
+      location: params[:filter][:location],
+      capacity: params[:filter][:capacity]
+    )
+    ​
+    @restaurants =  if params[:filter]
+      Hotel.where(
+        location: params[:filter][:location],
+                  end
+    ​
+    @activities =  if params[:filter]
+      Hotel.where(
+        location: params[:filter][:location],
+        toto: params[:filter][:toto]
+        # capacity: params[:filter][:capacity]
+        )
+      else
+        []
+      end
+  end
+
   def about
   end
 
