08655a33d34a83993dfba0568dc2a31ca23ea0aa
Wila Bertrand <wilaury.bertrand@acanopya.com>
2020-05-28 11:43:22 +0200

    some changes in the Home and add the gitignore to hide some data

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,56 +1,56 @@
 class TripsController < ApplicationController
-    before_action :set_trip, only: [:show, :update, :edit, :destroy]
+  before_action :set_trip, only: [:show, :update, :edit, :destroy]
 
-    def new
-        @trip = current_user.trips.new
-        authorize_trip
-    end
+  def new
+      @trip = current_user.trips.new
+      authorize_trip
+  end
 
-    def index
-        @trips = policy_scope(Trip).order(created_at: :desc)
-    end
+  def index
+      @trips = policy_scope(Trip).order(created_at: :desc)
+  end
 
-    def create
-        @trip = current_user.trips.new(trip_params)
-        authorize_trip
+  def create
+      @trip = current_user.trips.new(trip_params)
+      authorize_trip
 #       if policy(Trip).create?
-        if @trip.save
-            flash[:notice] = "Votre voyage a bien été créé"
-            redirect_to flats_path
-        else
-            flash[:alert] = "Votre voyage ne s'est pas bien créé"
-            render :new
-        end
-    end
-    
-    def show
-    end
+      if @trip.save
+          flash[:notice] = "Votre voyage a bien été créé"
+          redirect_to flats_path
+      else
+          flash[:alert] = "Votre voyage ne s'est pas bien créé"
+          render :new
+      end
+  end
 
-    def edit
-    end
+  def show
+  end
 
-    def update
-        @trip.update(trip_params)
-        redirect_to trip_path(@trip)
-    end
+  def edit
+  end
 
-    def destroy
-        @trip.destroy
-        redirect_to trips_path
-    end
+  def update
+      @trip.update(trip_params)
+      redirect_to trip_path(@trip)
+  end
 
-    private
+  def destroy
+      @trip.destroy
+      redirect_to trips_path
+  end
 
-    def authorize_trip
-        authorize(@trip)
-    end
+  private
 
-    def trip_params
-        params.require(:trip).permit(:name, :location, :start_date, :end_date, :number_of_travellers)
-    end
+  def authorize_trip
+      authorize(@trip)
+  end
 
-    def set_trip
-        @trip = Trip.find(params[:id])
-        authorize_trip
-    end
+  def trip_params
+      params.require(:trip).permit(:name, :location, :start_date, :end_date, :number_of_travellers)
+  end
+
+  def set_trip
+      @trip = Trip.find(params[:id])
+      authorize_trip
+  end
 end
