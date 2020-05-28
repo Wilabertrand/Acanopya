1921181448d7d09f07f117a1aa59876d881b9051
Astrid Boisson <astrid.bdc@gmail.com>
2020-05-28 17:11:17 +0200

    correctifs commentaires

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,42 +5,6 @@ class PagesController < ApplicationController
     @trip = Trip.new
   end
 
-  def dashboard
-    
-    if params[:query].present?
-      @flats = Flat.where("address ILIKE ?", "%#{params[:query]}%") # && Flat.where(capacity: params[:query])
-    else
-      @flats = policy_scope(Flat).order(created_at: :desc)
-    end
-
-    #  @flats =  if params[:filter]
-    #             Flat.where(
-    #             address: params[:filter][:address],
-    #             capacity: params[:filter][:capacity]
-    #             )
-    #           else
-    #             @flats = policy_scope(Flat).order(created_at: :desc)
-    #           end
-
-  #   @restaurants = if params[:query]
-  #                   Restaurant.where(
-  #                     address: params[:query][:address],
-  #                     )
-  #                   else
-  #                     @restaurants = policy_scope(Restaurant).order(created_at: :desc)
-  #                   end
-  # ​
-  #   @activities = if params[:query]
-  #                   Activity.where(
-  #                   address: params[:query][:address],
-  #                   )
-  #                 else
-  #                   @activities = policy_scope(Activity).order(created_at: :desc)
-  #                 end
-  
-   # redirect_to flats_path
-  end
-
   def about
   end
 
