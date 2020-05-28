cc634746caf1395151195e8092adac1915001f5f
Harold <h.ngano@gmail.com>
2020-05-28 18:20:32 +0200

    everything fixed

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -25,14 +25,6 @@ require("channels")
 // External imports
 import "bootstrap";
 
-// Internal imports, e.g:
-// import { initSelect2 } from '../components/init_select2'; 
-
-document.addEventListener('turbolinks:load', () => {
-  // Call your functions here, e.g:
-  // initSelect2();
-});
-
 // MAPBOX
 import { initMapbox } from '../plugins/init_mapbox';
 import { initAutocomplete } from '../plugins/init_autocomplete';
