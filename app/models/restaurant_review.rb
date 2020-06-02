44db952fed2db18747caf3333ead08c074a324b4
Astrid Boisson <astrid.bdc@gmail.com>
2020-06-02 15:56:24 +0200

    few updates3

--- app/models/restaurant_review.rb
+++ app/models/restaurant_review.rb
@@ -4,4 +4,14 @@ class RestaurantReview < ApplicationRecord
 
   validates :rating,  numericality: { greater_than: 0, less_than_or_equal_to: 5 }, allow_blank: true
 
+  def average_rating
+    ratings = []
+    ratings << rating
+    sum = 0
+    ratings.each do |rating|
+      sum += rating
+    end
+    @average_rating = sum / RestaurantReview.count
+  end
+
 end
