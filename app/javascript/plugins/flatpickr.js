4b27a3b01e9e63b8cffc1ff7c6147d57d1bea906
Wila Bertrand <wilaury.bertrand@acanopya.com>
2020-06-02 10:56:55 +0200

    form already done

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -1,9 +1,3 @@
 import flatpickr from "flatpickr";
-// import rangePlugin from "flatpickr/dist/plugins/rangePlugin";
 
 flatpickr(".datepicker", {});
-
-// flatpickr("#range_start", {
-//   altInput: true,
-//   plugins: [new rangePlugin({ input: "#range_end"})]
-// });
