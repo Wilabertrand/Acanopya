ff795e2327fab247ab8989b420521719b48e5242
Astrid Boisson <astrid.bdc@gmail.com>
2020-06-04 10:22:28 +0200

    ok pending

--- db/migrate/20200604061456_add_column_to_booking_flats.rb
+++ db/migrate/20200604061456_add_column_to_booking_flats.rb
@@ -1,5 +1,5 @@
 class AddColumnToBookingFlats < ActiveRecord::Migration[6.0]
   def change
-    add_column :booking_flats, :status_reservation, :string
+    add_column :booking_flats, :status_reservation, :string, default: "pending"
   end
 end
--- db/migrate/20200604070114_add_column_to_booking_restaurants.rb
+++ db/migrate/20200604070114_add_column_to_booking_restaurants.rb
@@ -1,5 +1,5 @@
 class AddColumnToBookingRestaurants < ActiveRecord::Migration[6.0]
   def change
-    add_column :booking_restaurants, :status_reservation, :string, default: "en attente de confirmation"
+    add_column :booking_restaurants, :status_reservation, :string, default: "pending"
   end
 end
